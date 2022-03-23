5f27c150899775704c5e9d73112bc5a594516864
Peter McClelland <peterwmcclelland@gmail.com>
2022-03-23 10:06:28 -0500

    Trying to display the data

--- assets/js/vacation-vibes.js
+++ assets/js/vacation-vibes.js
@@ -1,27 +1,32 @@
 
 
-document.querySelector('#city-search').addEventListener('submit', function(event){
-    event.preventDefault()
-    var city = document.querySelector('#user-supply-city').value
+  const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com',
+		'X-RapidAPI-Key': '90100850f4mshed0f09a102cd133p1e45e7jsnbead281b9e76'
+	}
+};
 
-    console.log(city)
+fetch('https://wft-geo-db.p.rapidapi.com/v1/geo/cities?location=30.266666-97.73', options)
+	.then(response => response.json())
+	.then(response => console.log(response))
+	.catch(err => console.error(err))
 
-    fetch(`https://instagram47.p.rapidapi.com/location_search?search=${city}`, {
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "instagram47.p.rapidapi.com",
-            "x-rapidapi-key": "90100850f4mshed0f09a102cd133p1e45e7jsnbead281b9e76"
-        }
-    })
-    
-    
-    .then(response => {
-        return response.json()
-    })
-    .then(data => {
-        console.log(data.body);
-    })
-    .catch(err => {
-        console.error(err);
-    });
-})
+        .then(data => {
+            console.log(data.body.links)
+        })
+            
+
+
+
+        // .then(response => {
+            //     return response.json()
+            // })
+            // .then(data => {
+            //     console.log(data.body.id)
+            // })
+            // .catch(err => {
+            //      console.error(err)
+            // })
+            
